41edf53b026f45d15f3656c2176301fc7aa8269d
ktse <kendricktse888@gmail.com>
2017-06-03 11:57:51 -0700

    Week 3 Causer R programming assignment

---
+++ cachematrix.R
@@ -0,0 +1,36 @@
+## These functions finds the inverse of an invertible matrix 
+
+## This function defines the set, get, setinverse and getinverse
+## functions and returns a list of these functions.
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## This function utilizes the functions that are returned in 
+## makeCacheMatrix and gets the inverse matrix using the 
+#solve function
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
